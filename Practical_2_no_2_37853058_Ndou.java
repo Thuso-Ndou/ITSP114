a3657645ac92069aebd351be03cace060bf7aa95
Thuso Ndou <104587370+Thuso-Ndou@users.noreply.github.com>
2023-05-25 21:56:27 +0200

    Update Practical_2_no_2_37853058_Ndou.java

--- Practical_2_no_2_37853058_Ndou.java
+++ Practical_2_no_2_37853058_Ndou.java
@@ -1,6 +1,6 @@
 
 /**
- * Write a description of class Practical_2_no_2_37853058_Ndou here.
+ * A java program that prompt for name, surname and student number then displays the details.
  *
  * @author (Thuso Ndou)
  * @version (114/03/2023)
